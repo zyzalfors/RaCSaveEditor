3ae42b19d695684f451349364492c5c41e8419df
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2026-01-27 09:38:59 +0100

    Add files via upload

--- Combobox.py
+++ Combobox.py
@@ -3,12 +3,11 @@ from tkinter import ttk
 
 class Combobox(ttk.Combobox):
 
-    def __init__(self, root, name, x, y, val, vals):
+    def __init__(self, root, name, val, vals):
         self.var = tkinter.StringVar()
         self.var.set(val)
-        super().__init__(root, height = 1, width = 41, textvariable = self.var, state = "readonly")
+        super().__init__(root, height = 1, width = 30, textvariable = self.var, state = "readonly")
         self.name = name
-        self.place(anchor = "nw", x = x, y = y)
         self["values"] = tuple(vals)
 
 
